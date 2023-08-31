8e3ce2e4532139003b4ba127818fcebdd366a98d
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-08-31 14:10:11 +0630

    Symbol

--- app.js
+++ app.js
@@ -25,4 +25,25 @@ var obj2 = {}
 // obj.logMe();
 // obj.logStillMe();
 // obj.logChangedMe();
-logThis.call(obj2);
+// logThis.call(obj2);
+
+
+var myObj = {}
+
+myObj[myObj] = "Hello Who are you?";
+
+var symb = Symbol("id");
+var uiquesSymb = Symbol("id");
+
+var myId = "ID";
+var myKey = "ID";
+
+myObj[symb] = "eh";
+myObj[uiquesSymb] = "eh";
+
+
+console.log(myObj[symb]);
+console.log(myObj[uiquesSymb])
+
+
+
