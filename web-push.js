ed30599ac1c79db4f4a7e9457ebc784a41a6946f
Yan Moe Naing <yanmoenaing@yomabank.com>
2023-10-02 13:17:39 +0630

    web

--- web-push.js
+++ web-push.js
@@ -14,12 +14,12 @@ webpush.setVapidDetails(
 
 const sub = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/cNBJIQ5-ips:APA91bErqSGE58y5YDBDTEismRBibxRW4bwoOcO4dvzxW4naFHn63m2vMuNBLecZoj_92lxo5a9bu_CWmDOzRIyIfqvSOJyzqFRyc_u476oTdiExQK-vDlmSgRJR3vAm9q9vcSEXju8G",
+    "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABlGlAsGc34NzeIs1UId8ns_IrnAwv8wSFwFsa6MekB-g_d3VLKsG_akN98xBS-oOVupAHQpr0f0y71jiwqjBjskpYJQsMIzHJ3Ng4kcfOtNGZHuzKQSE6Jhef2hHv5KxO7NukBgyE2qNc1_3Udu4NvI2kitLLia6SmEmjMEItc8d7Oc9Q",
   expirationTime: null,
   keys: {
+    auth: "m9f0W_jScP5OhkhsfLP5aA",
     p256dh:
-      "BIsKndJosfIge88XbA6q-KHFkRu7CL0mXyQuCbWe8dMcfy3yfH_QZ3NImTsrZ0G1Ya3dXipx3J90lhCHExCaSRo",
-    auth: "jonnN56HLdd6YdeiQqp4Yw",
+      "BB1lGreLs91Csv12W-Q08buZinaggjGWtN8nMkU4vqqoItvu1qdFv39pPdYa_KBkcd3SBq9aDFW-VU4nXxXWi_4",
   },
 };
 webpush.sendNotification(sub, "My dearest Go Youn Jung");
