24fcd25ff38846c8c5c42f6edd5768f342344e96
Yan Moe Naing <yanmoenaing@yomabank.com>
2023-10-02 11:26:04 +0630

    save

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 self.addEventListener("push", (event) => {
   event.waitUntil(
     self.registration.showNotification("My Beloved Go Youn Jung", {
-      body: " Go Youn-jung is a South Korean actress and model signed under MAA. She made her acting debut in the television series He Is Psychometric and gained recognition for her supporting role in the Netflix series Sweet Home.",
-      icon: "go-yoon-jung-drama.jpg",
-      vibrate: [200, 100, 200, 100, 200, 100, 200],
-      tag: "go-youn-jung",
+      body: " Go Youn-jung is a South Korean actress and model signed under MAA. She made her acting debut in the television series He Is Psychometric and gained recognition for her supporting role in the Netflix series Sweet Home."
     })
   );
 });
--- web-push.js
+++ web-push.js
@@ -6,8 +6,20 @@ const vapidkeys = {
   privateKey: "-OYlsLf01944XxtHq_2fMRZb9a5T1Yf_hxGFAiIrPJo",
 };
 
+webpush.setVapidDetails(
+  "mailto:test@naing.com",
+  vapidkeys.publicKey,
+  vapidkeys.privateKey
+);
 
-webpush.setVapidDetails('mailto:test@naing.com', vapidkeys.publicKey, vapidkeys.privateKey);
-
-const sub = {};
-webpush.sendNotification(sub, 'My dearest Go Youn Jung');
+const sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/dW5gk41d8_0:APA91bFI-p5f9No9otZljMOvD9sUFIrXCHb7licvHxcvNFkg0crXv-90Yow3bZf33w_2vzX5tHVjHzC7NnzXJGENy-KLvvK6ODwTq7jtt3oqTPaShMDCOjeYMlZzxWRpFMXDkM9z33oA",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BI9o_h8yDoAT8wPE07U8PwcJ_m2U_SmYSMPbEYXc4SeWb494XSQT8oujUuHgEPubEBfyBkfiWqrkSEuboiurDOo",
+    auth: "PC8HeexUoZrVUAhezSpCGg",
+  },
+};
+webpush.sendNotification(sub, "My dearest Go Youn Jung");
